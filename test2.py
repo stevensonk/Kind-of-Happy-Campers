a075ac9e20e54967a0619a7267c4142e6d35ce33
archulca <46728805+archulca@users.noreply.github.com>
2022-01-05 15:44:42 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,10 +16,12 @@ class Paint(object):
         self.brush_button = Button(self.root, text='brush', command=self.use_brush)
         self.brush_button.grid(row=0, column=1)
 
-        self.color_button = Button(self.root, text='color', command=self.choose_color)
+        color_button_png = PhotoImage(file= 'color_button.png')
+        self.color_button = Button(self.root, image = color_button_png, command=self.choose_color)
         self.color_button.grid(row=0, column=2)
 
-        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        eraser_button_png = PhotoImage(file = 'eraser_button.png')
+        self.eraser_button = Button(self.root, image = eraser_button_png, text='eraser', command=self.use_eraser)
         self.eraser_button.grid(row=0, column=3)
 
         self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
